090e67f3c46a75085aa47cc5186f4bb54efd2a5e
MartinLundqvist <iphonelynden@gmail.com>
2025-11-03 14:50:28 +0100

    Dynamic back-end porting

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist/client',
-    sourcemap: true,
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: process.env.API_URL || 'http://localhost:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  console.log(env);
+  const port = env.PORT || 3000;
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "dist/client",
+      sourcemap: true,
+    },
+    server: {
+      port: 5174,
+      proxy: {
+        "/api": {
+          target: `http://localhost:${port}`,
+          changeOrigin: true,
+        },
       },
     },
-  },
-})
+  };
+});
